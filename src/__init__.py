6d45f5a38af274024a280920bedbef8d8ad132cc
Jdsatashi <vdt1073@gmail.com>
2023-12-06 13:19:54 +0700

    revert

--- src/__init__.py
+++ src/__init__.py
@@ -17,12 +17,12 @@ app.config.from_mapping(
 #
 bcrypt = Bcrypt(app)
 
-from src.requests.role import add_default_role, create_admin_account
+from .requests.role import add_default_role, create_admin_account
 
 add_default_role()
 create_admin_account()
 # Add all routes
-from src import routes
+from . import routes
 
 # Register prefix
 app.register_blueprint(routes.auth_route.auth, url_prefix='/auth')
