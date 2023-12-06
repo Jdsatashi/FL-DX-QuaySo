eb7eb241be0299eb38facaaa6b35df71f4f25381
Jdsatashi <vdt1073@gmail.com>
2023-12-06 13:16:40 +0700

    Change destination __init__.py

--- __init__.py
+++ __init__.py
@@ -17,12 +17,12 @@ app.config.from_mapping(
 #
 bcrypt = Bcrypt(app)
 
-from .requests.role import add_default_role, create_admin_account
+from src.requests.role import add_default_role, create_admin_account
 
 add_default_role()
 create_admin_account()
 # Add all routes
-from . import routes
+from src import routes
 
 # Register prefix
 app.register_blueprint(routes.auth_route.auth, url_prefix='/auth')
