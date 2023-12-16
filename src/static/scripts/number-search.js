3026411c4aa3eb7782139176675c7ca215a4ab03
Jdsatashi <vdt1073@gmail.com>
2023-12-16 23:27:46 +0700

    Optimize files

--- src/static/scripts/number-search.js
+++ src/static/scripts/number-search.js
@@ -1,79 +1,191 @@
-import addElement from "./helper.js";
+import addElement from "../../static/scripts/helper.js";
+
 const inputSearch = document.querySelector('#input-search-number');
 const showListSelected = document.querySelector('#show-selected-number')
 const groupData = document.querySelector('#group-data')
 let listItem = groupData.querySelectorAll('.group-item')
 const userId = document.getElementById('user-id')
+const turnChoose = parseInt(document.getElementById('turn_choose').innerText)
+const hiddenPlace = document.getElementById('hidden-selected')
+const giftRuleBtn = document.getElementById('giftRule')
+const randomNumber = document.getElementById('randNum')
+let randomLoop = 0
 
-// Live searching
+giftRuleBtn.onclick = function () {
+    let giftRuleImg = document.getElementById('giftRuleImage')
+    giftRuleImg.style.display = 'block'
+}
+
+randomNumber.onclick = RandomChoice
+
+// Live searching navigate function
 let inputSearchEvent = (e) => {
     liveSearch(e, listItem, showListSelected)
 }
-
+// Live search Event
 inputSearch.addEventListener('input', inputSearchEvent)
-
+// Live search main function
 listItem.forEach((item) => {
     item.onclick = () => {
         let item_value = item.textContent.trim()
-        clickAddItem(item, item_value, showListSelected)
+        let limit = checkLimit()
+        if (limit) {
+            alert(`Bạn đã chọn ${turnChoose} số, loại bỏ số hiện tại để chọn số mới.`)
+        } else {
+            clickAddItem(item, item_value)
+        }
     }
 })
 
-function liveSearch(e, listItem, showListSelected) {
+// Get selected number
+const placeSelecting = document.getElementById('item-selecting')
+const selecting = placeSelecting.querySelectorAll('.group-item')
+// Show selected number to chosen place function
+selecting.forEach(item => {
+    let item_value = item.textContent.trim()
+    clickAddItem(item, item_value, showListSelected)
+})
+
+// Function handling search item
+function liveSearch(e, listItem) {
     let value = e.target.value.toLowerCase()
     listItem.forEach((item) => {
         let item_value = item.textContent.trim()
         if (item_value.includes(value)) {
-            item.style.display = 'block'
-            item.onclick = function () {
-                clickAddItem(item, item_value, showListSelected)
+            let arrCurrent = currentSelecting()
+            if (!arrCurrent.includes(item_value)) {
                 setTimeout(() => {
-                    e.target.value = ''
-                    listItem.forEach(item => {
-                        item.style.display = 'block'
-                    })
-                }, 100)
+                    item.style.display = 'block'
+                }, 1)
             }
+            item.onclick = onClickLiveSearch
         } else {
-            item.style.display = 'none'
+            setTimeout(() => {
+                item.style.display = 'none'
+            }, 1)
         }
     })
 }
 
-
-function clickAddItem(item, item_value, showListSelected) {
-    let addItem = addElement(showListSelected, {
-        createTag: 'p',
-        options: {
-            id: `li-result-${item_value}`,
-            className: 'item-card',
-            title: `Số ${item_value.toString()}`,
-            innerHTML: `<span class="item-list">${item_value.toString()}</span>`
+function RandomChoice(e) {
+    randomLoop++
+    let min = 1;
+    let max = Array.from(listItem).length;
+    let random = Math.floor(Math.random() * (max - min + 1) + min)
+    let arrResult = currentSelecting()
+    if (arrResult.includes(random.toString())) {
+        if (arrResult.length < max) {
+            console.log("Trùng số: " + random + " rerandom")
+            return RandomChoice(e)
+        } else {
+            randomLoop = 0
+            alert("Không thể chọn ngẫu nhiên, vui lòng thử lại")
         }
-    })
+    } else {
+        let limit = checkLimit(e)
+        if (limit) {
+            alert(`Bạn đã chọn ${turnChoose} số, loại bỏ số hiện tại để chọn số mới.`)
+        } else {
+            let item = Array.from(listItem)[random]
+            try {
+                let item_value = item.textContent.trim()
+                clickAddItem(item, item_value, showListSelected)
+            } catch (e) {
+                randomLoop = 0
+                alert("Không thể chọn ngẫu nhiên, vui lòng thử lại")
+            }
 
-    let removeButton = addElement(addItem, {
-        createTag: 'button',
-        options: {
-            id: `btn-remove-${item_value}`,
-            className: 'item-button',
-            title: `Bỏ chọn số ${item_value.toString()}`,
-            innerHTML: `
-                    <svg class="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                         fill="none"
-                         viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                              stroke-width="2"
-                              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"></path>
-                    </svg>`
         }
-    })
-    const removeBtnE = document.getElementById(removeButton.id)
-    removeBtnE.onclick = function (evn) {
-        evn.preventDefault()
-        addItem.remove()
-        removeButton.remove()
-        item.style.display = 'block'
     }
+}
+
+function onClickLiveSearch(e) {
+    let limit = checkLimit()
+    if (limit) {
+        alert(`Bạn đã chọn ${turnChoose} số, loại bỏ số hiện tại để chọn số mới.`)
+    } else {
+        clickAddItem(e.target, e.target.innerHTML.trim(), showListSelected)
+    }
+
+    setTimeout(() => {
+        inputSearch.value = ''
+    }, 1)
+    listItem.forEach(item => {
+        let arrResult = currentSelecting()
+        if (arrResult.includes(item.innerText.trim())) {
+            arrResult.splice(arrResult.indexOf(item.innerText.trim()), 1)
+        } else {
+            setTimeout(() => {
+                item.style.display = 'block'
+            }, 1)
+        }
+    })
+}
+
+// Show item selected place at choosing position
+function updatePlaceSelectingDisplay() {
+    let hasDisplayedItem = false;
+    selecting.forEach(item => {
+        if (item.style.display === 'block') {
+            hasDisplayedItem = true;
+        }
+    });
+    placeSelecting.style.display = hasDisplayedItem ? 'block' : 'none';
+}
+
+// Function handle clicking to add item to place showing selected item
+function clickAddItem(item, item_value, showListSelected) {
     item.style.display = 'none'
+    let arrResult = currentSelecting()
+    if (arrResult.includes(item_value)) {
+        alert(`Không thể chọn số ${item_value}`)
+    } else {
+        let addItem = addElement(showListSelected, {
+            createTag: 'p',
+            options: {
+                id: `li-result-${item_value}`,
+                className: 'item-card',
+                title: `Số ${item_value.toString()}`,
+                innerHTML: `<span class="item-list">${item_value.toString()}</span>`
+            }
+        })
+
+        let removeButton = addElement(addItem, {
+            createTag: 'button',
+            options: {
+                id: `btn-remove-${item_value}`,
+                className: 'item-button',
+                title: `Bỏ chọn số ${item_value.toString()}`,
+                innerHTML: xButton
+            }
+        })
+        const removeBtnE = document.getElementById(removeButton.id)
+        removeBtnE.onclick = function (evn) {
+            evn.preventDefault()
+            addItem.remove()
+            removeButton.remove()
+            item.style.display = 'block'
+            updatePlaceSelectingDisplay()
+        }
+        updatePlaceSelectingDisplay()
+    }
+}
+
+function checkLimit() {
+    let limit = false
+    let numberChosen = showListSelected.querySelectorAll("p.item-card")
+    numberChosen = Array.from(numberChosen).length
+    if (numberChosen >= turnChoose) {
+        limit = true
+    }
+    return limit
+}
+
+function currentSelecting() {
+    let groupChosen = showListSelected.querySelectorAll('.item-card')
+    let arrResult = []
+    for (let i of groupChosen) {
+        arrResult.push(i.innerText.trim())
+    }
+    return arrResult
 }
