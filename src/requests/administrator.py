9e89e0aea10ff4af0e64a5997a3efd1791de7400
Jdsatashi <vdt1073@gmail.com>
2023-12-19 22:07:42 +0700

    Change account data, add point and user-code, remove email, add user profile

--- src/requests/administrator.py
+++ src/requests/administrator.py
@@ -5,7 +5,7 @@ from src.requests.authenticate import admin_authorize
 from src.forms import CreateAccountForm, UpdateAccountForm
 from src.models import Models
 from src.mongodb import ACCOUNT_TABLE
-from src.utils.utilities import role_auth_id, validate_account_create
+from src.utils.utilities import role_auth_id
 from src.requests.event import event_model, join_event_model
 
 import bcrypt
@@ -54,7 +54,8 @@ def account_create():
 
         form_user = {
             "username": str(form.username.data).lower(),
-            "email": form.email.data,
+            "usercode": form.usercode.data,
+            "point": form.point.data,
             "password": hashed_password,
             "role_id": role_auth_id,
             "is_active": True,
@@ -62,23 +63,27 @@ def account_create():
         }
         events_join = form.join_event.data
 
-        validate_account_create(form_user['email'], form_user['username'])
+        if ACCOUNT_TABLE.find_one(form_user['username']):
+            flash(f"Username đã tồn tại.", "warning")
+            return redirect(url_for('admin.account_create'))
+
         if form.validate_on_submit():
             try:
                 account.create(form_user)
-                user = account.get_one({"username": form.username.data, "email": form.email.data})
+                user = account.get_one({"username": form.username.data, "point": form.point.data})
                 user['_id'] = str(user['_id'])
+                user['point'] = int(user['point'])
                 if events_join:
                     events_join = events_join.split('|')
                     for event in events_join:
-                        event = event.split(':')
+                        event_data = event_model.get_one({'_id': ObjectId(event)})
+                        turn_roll = user['point'] // int(event_data['point_exchange'])
                         input_data = {
                             'user_id': user['_id'],
-                            'event_id': event[0],
-                            'turn_roll': int(event[1])
+                            'event_id': event,
+                            'turn_roll': turn_roll
                         }
                         join_event_model.create(input_data)
-
                 print(f"Created successfully {form_user['username']}.")
                 flash(f"Tạo thành công tài khoản '{form_user['username']}'.", "success")
                 return redirect(url_for('home'))
@@ -92,12 +97,18 @@ def account_create():
 
 @admin.route('account/<string:_id>', methods=['POST', 'GET'])
 def account_edit(_id):
+    # Authorize is admin
     adm = admin_authorize()
+    if not adm:
+        flash("Bạn không được phép truy cập vào trang này.", 'danger')
+        return redirect(url_for('home'))
+    # Get data were created
     events = list(event_model.get_all())
     user_joins = list(join_event_model.get_many({'user_id': _id}))
     event_not_join = list()
     event_join = dict()
 
+    # Create event_join dict to store require data to showing
     for event in user_joins:
         event_join.update({event['event_id']: {}})
     for event in events:
@@ -110,20 +121,20 @@ def account_edit(_id):
     for event in user_joins:
         if event['event_id'] in event_join:
             event_join[event['event_id']].update({'turn_roll': event['turn_roll']})
-
-    if not adm:
-        flash("Bạn không được phép truy cập vào trang này.", 'danger')
-        return redirect(url_for('home'))
+    # Get form data
     form = UpdateAccountForm()
+    # Get user data for updating
     user = account.get_one({'_id': ObjectId(_id)})
     if not user:
         flash('Không tìm thấy tài khoản.', 'warning')
         return redirect(url_for('home'))
+    # Update user method
     if request.method == 'POST':
         events_join = form.join_event.data
         form_data = {
             "username": str(form.username.data).lower(),
-            "email": form.email.data,
+            "usercode": form.usercode.data,
+            "point": form.point.data,
             "is_active": form.is_active.data,
             "date_updated": datetime.utcnow()
         }
@@ -133,19 +144,27 @@ def account_edit(_id):
                 account.update(ObjectId(_id), {'date_updated': ''})
             try:
                 edit_account = account.update(ObjectId(_id), form_data)
-                events_join = events_join.split('|')
-                for event in events_join:
-                    event = event.split(':')
-                    input_data = {
-                        'user_id': _id,
-                        'event_id': event[0],
-                        'turn_roll': event[1]
-                    }
-                    has_exists = join_event_model.get_one({'event_id': event[0], 'user_id': _id})
-                    if has_exists is None:
-                        join_event_model.create(input_data)
-                    else:
-                        join_event_model.update(has_exists['_id'], input_data)
+                user['point'] = int(user['point'])
+                if events_join:
+                    events_join = events_join.split('|')
+                    print("Event join: ", events_join)
+                    for event in events_join:
+                        print("Event each: ", event)
+                        event_data = event_model.get_one({'_id': ObjectId(event)})
+                        print("Event data: ", event_data)
+                        turn_roll = user['point'] // int(event_data['point_exchange'])
+                        print("Turn roll: ", turn_roll)
+                        input_data = {
+                            'user_id': user['_id'],
+                            'event_id': event,
+                            'turn_roll': turn_roll
+                        }
+                        print("Input data: ", input_data)
+                        has_exists = join_event_model.get_one({'event_id': event[0], 'user_id': _id})
+                        if has_exists is None:
+                            join_event_model.create(input_data)
+                        else:
+                            join_event_model.update(has_exists['_id'], input_data)
                 flash(f'Cập nhật tài khoản "{edit_account["username"]}".', 'success')
                 return redirect(url_for('admin.account_manager'))
             except Exception as e:
