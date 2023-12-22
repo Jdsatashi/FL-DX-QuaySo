e74a7f74ec8fc25e8f4e13fc8b6af5efadb4c83e
Jdsatashi <vdt1073@gmail.com>
2023-12-22 11:00:25 +0700

    Update auto update status for event

--- src/app.py
+++ src/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
-import os
 from dotenv import load_dotenv
 from _datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+
+import os
+
 
 # Loading environment variables
 load_dotenv()
 
 # os.add_dll_directory(r"C:\Program Files\GTK3-Runtime Win64\bin")
+
+scheduler = BackgroundScheduler()
 
 # Create logs folder
 if not os.path.exists("logs"):
@@ -56,3 +61,30 @@ from src import routes
 app.register_blueprint(routes.auth_route.auth, url_prefix='/user')
 app.register_blueprint(routes.admin_route.admin, url_prefix='/admin')
 app.register_blueprint(routes.event.events, url_prefix='/events')
+
+
+from src.models import Models
+from src.mongodb import EVENT_TABLE
+
+
+def check_active():
+    event_model = Models(table=EVENT_TABLE)
+    events = event_model.get_all()
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    for event in events:
+        form_date = event['date_close']
+        is_active = False if current_date > form_date else True
+        event_model.update(event['_id'], {'is_active': is_active})
+        logs.message_logger.info(f"Update {event['event_name']} daily.")
+
+
+scheduler.add_job(
+    func=check_active,
+    trigger="cron",
+    hour="0",
+    minute="0",
+    timezone="Asia/Ho_Chi_Minh",
+)
+
+# Run scheduler
+scheduler.start()
