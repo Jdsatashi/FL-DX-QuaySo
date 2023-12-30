5bec4c68397d2cfdff8f88250270f5850aac6735
Jdsatashi <vdt1073@gmail.com>
2023-12-30 22:35:15 +0700

    Update logs error and event detail

--- src/requests/event.py
+++ src/requests/event.py
@@ -8,6 +8,8 @@ from src.models import Models
 from src.mongodb import EVENT_TABLE, USER_JOIN_EVENT
 from src.requests.authenticate import admin_authorize
 
+import traceback
+
 events = Blueprint('event', __name__)
 event_model = Models(table=EVENT_TABLE)
 join_event_model = Models(table=USER_JOIN_EVENT)
@@ -28,22 +30,28 @@ def index():
             event['date_close'] = event['date_close'].strftime('%Y-%m-%d')
         except AttributeError:
             event['date_close'] = event['date_close']
-    return render_template('events/index.html', events=data_list, title="Quản lý sự kiện")
+    return render_template('admin/events/index.html', events=data_list, title="Quản lý sự kiện")
 
 
 @events.route('/create', methods=['POST', 'GET'])
 def insert():
+    # Authorize is admin
     is_admin = admin_authorize()
     if not is_admin:
         return redirect(url_for('home'))
+    # Get form rule
     form = EventForm()
-    if request.method == 'GET':
-        now = datetime.now().strftime('%Y-%m-%d')
-        return render_template('events/create.html', form=form, date_now=now, title="Thêm sự kiện")
+    # Get current datetime for checking status in template
+    now = datetime.now().strftime('%Y-%m-%d')
     if request.method == 'POST':
-        current_date = datetime.now().strftime('%Y-%m-%d')
+        # Get current datetime when updating
+        current_time = datetime.now()
+        # Format datetime now compare with date close
+        current_date = current_time.strftime('%Y-%m-%d')
         form_date = form.date_close.data.strftime('%Y-%m-%d')
+        # Status will activate when date close smaller than current date
         is_active = False if current_date > form_date else True
+        # Create data dict
         data_form = {
             'event_name': form.name.data,
             'limit_repeat': abs(int(form.repeat_limit.data)),
@@ -51,38 +59,49 @@ def insert():
             'date_start': form.date_start.data.strftime('%Y-%m-%d'),
             'date_close': form.date_close.data.strftime('%Y-%m-%d'),
             'is_active': is_active,
-            'date_created': datetime.utcnow()
+            'date_created': current_time
         }
+        # Validate form data
         if form.validate_on_submit():
+            # Process data to database
             try:
                 event_model.create(data_form)
                 flash(f'Tạo thành công "{data_form["event_name"]}".', 'success')
                 return redirect(url_for('event.index'))
+            # Except error
             except Exception as e:
-                print("Error.", e)
+                error_info = traceback.format_exc()
+                logger.error(f'Error creating event.\nError: {e}\n{error_info}"')
                 flash('Server gặp sự cố, vui lòng thử lại sau.', 'warning')
-                return redirect(url_for('event.index'))
-        return redirect(url_for('home'))
+        flash('Tạo tài khoản thất bại, vui lòng kiểm tra lại thông tin.', 'warning')
+        return redirect(url_for('event.index'))
+    return render_template('admin/events/create.html', form=form, date_now=now, title="Thêm sự kiện")
 
 
 @events.route('edit/<string:_id>', methods=['POST', 'GET'])
 def update(_id):
+    # Authorize is admin
     is_admin = admin_authorize()
     if not is_admin:
         return redirect(url_for('home'))
+    # Get specific event for updating
     spec_event = event_model.get_one({'_id': ObjectId(_id)})
+    # Try to change datetime format
     try:
         spec_event['date_close'] = spec_event['date_close'].strftime('%Y-%m-%d')
     except AttributeError:
         spec_event['date_close'] = spec_event['date_close']
+    # Get form field and data
     form = EventForm()
+    # If specific event existed
     if spec_event:
-        if request.method == 'GET':
-            return render_template('events/edit.html', form=form, event=spec_event, title="Sửa sự kiện")
-        elif request.method == 'POST':
+        if request.method == 'POST':
+            # Get current datetime format to compare with date close
             current_date = datetime.now().strftime('%Y-%m-%d')
             form_date = form.date_close.data.strftime('%Y-%m-%d')
+            # Status will activate when date close smaller than current date
             is_active = False if current_date > form_date else form.is_active.data
+            # Create data dict
             data_form = {
                 'event_name': form.name.data,
                 'limit_repeat': abs(int(form.repeat_limit.data)),
@@ -91,17 +110,35 @@ def update(_id):
                 'is_active': is_active,
                 'date_created': datetime.utcnow()
             }
+            # Validate form fields
             if form.validate_on_submit():
+                # Try updating data to database
                 try:
                     event_model.update(ObjectId(_id), data_form)
                     flash(f'Cập nhật thành công "{data_form["event_name"]}".', 'success')
                     return redirect(url_for('event.index'))
+                # Except error
                 except Exception as e:
-                    logger.error("Error updating event. ", e)
+                    error_info = traceback.format_exc()
+                    logger.error(f'Error updating event.\nError: {e}\n{error_info}"')
             flash(f'Cập nhật thất bại, vui lòng kiểm tra lại.', 'danger')
             return redirect(url_for('event.update', _id=_id))
+        # Return GET method
+        return render_template('admin/events/edit.html', form=form, event=spec_event, title="Sửa sự kiện")
+    # Return event index when _id not found
+    logger.info(f"Cannot find event id: {_id}")
     flash(f'Không tìm thấy sự kiện.', 'warning')
     return redirect(url_for('event.index'))
+
+
+@events.route('event/detail/<string:_id>', methods=['GET'])
+def event_detail(_id, event_name):
+    # Authorize is admin
+    is_admin = admin_authorize()
+    if not is_admin:
+        return redirect(url_for('home'))
+    event_joined = join_event_model.get_many({'event_id': _id})
+    return render_template('admin/events/event_joins.html')
 
 
 # def saveFile():
