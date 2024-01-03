5b6818b2ac5cc21e4ec1d8c065d5c6b9afc22986
Jdsatashi <vdt1073@gmail.com>
2024-01-03 21:15:28 +0700

    Update pagination to model

--- src/models.py
+++ src/models.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Models:
     def __init__(self, table):
         self.table = table
@@ -11,9 +14,6 @@ class Models:
         else:
             return self.table.find(options)
 
-    def get_all_exclude(self, exclude):
-        return self.table.find(exclude)
-
     def get_many(self, data):
         return self.table.find(data)
 
@@ -23,13 +23,37 @@ class Models:
     def update(self, _id, data_dict):
         return self.table.find_one_and_update({
             '_id': _id
-            }, {
-                '$set': data_dict
-            }
-        )
+        }, {
+            '$set': data_dict
+        })
 
     def delete_one(self, _id):
         self.table.find_one_and_delete({'_id': _id})
 
     def delete_many(self, data_dict):
         pass
+
+    def pagination(self, current_page: int, perpage: int, query_data: dict, sorting: list = None):
+        table = self.table
+        if sorting is None:
+            sorting = []
+        if current_page <= 0:
+            current_page = 1
+        # Get total items in table
+        total_data = table.count_documents(query_data)
+        # From total items then calculate maximum page size | math.ceil make 4.8 to 5
+        total_pages = math.ceil(total_data / perpage)
+        # If current page is over max page then reset to max page
+        if current_page > total_pages:
+            current_page = total_pages
+        # Skip data
+        skip_data = (current_page - 1) * perpage
+        if skip_data < 0:
+            skip_data = 0
+        # Optional sorting or not
+        if sorting is not None and len(sorting) > 0:
+            data_list = list(table.find(query_data).sort(sorting).limit(perpage).skip(skip_data))
+        else:
+            data_list = list(table.find(query_data).limit(perpage).skip(skip_data))
+
+        return data_list, total_pages
